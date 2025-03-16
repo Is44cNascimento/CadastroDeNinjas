c0f75c6e6bb31426a5d59fc4c3cf91d3b29714ac
Is44cNascimento <isaacgamex7@outlook.com>
2025-03-16 20:50:12 -0300

    subiu

---
+++ src/test/java/java10x/CadastroDeNnjas/CadastroDeNnjasApplicationTests.java
@@ -0,0 +1,13 @@
+package java10x.CadastroDeNnjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNnjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
